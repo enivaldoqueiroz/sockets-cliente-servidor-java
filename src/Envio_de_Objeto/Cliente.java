ed963ed4228fda689160fa56cf0984ba8f761657
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-03-07 03:40:00 -0300

    Implementação das Classes Cliente, Pessoa e Servidor

---
+++ src/Envio_de_Objeto/Cliente.java
@@ -0,0 +1,24 @@
+package Envio_de_Objeto;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Cliente {
+
+    public static void main(String[] args) throws IOException {
+        //1 - Abrir conexão
+        Socket socket = new Socket("127.0.0.1", 54323);
+        
+        //2 - Definir stream de saída de dados do cliente
+        ObjectOutputStream saida = new ObjectOutputStream(socket.getOutputStream());
+        Pessoa p = new Pessoa("Enivaldo Queiroz", 29);
+        saida.writeObject(p);
+
+        //4 - Fechar streams de saída de dados
+        saida.close();
+
+        //5 - Fechar o socket
+        socket.close();
+    }
+}
